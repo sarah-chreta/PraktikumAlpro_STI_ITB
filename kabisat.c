f68982f45d6fcdbda3830ae75fc93c4307ad8b35
sarah-chreta <sarahchreta@gmail.com>
2026-03-07 23:39:24 +0700

    feat : Finalisasi Soal 1

--- kabisat.c
+++ kabisat.c
@@ -1,6 +1,35 @@
 /* File: ee */
 #include <stdio.h>
+
 int main (){
+    /* KAMUS */
+    /* Variabel */
+    int Y;
+
+    /* ALGORITMA */
+    do
+    {
+       scanf("%d", &Y);
+       if (Y <= 0){
+        printf("Masukkan tidak valid\n");
+    }
+    } while (Y <= 0);
+
+    if(Y % 400 != 0 && Y % 100 != 0 & Y % 4 == 0){
+        printf("Kabisat\n");
+    }
+
+    if(Y % 4 != 0){
+        printf("Bukan Kabisat\n");
+    }
+
+    if(Y % 400 == 0){
+        printf("Kabisat\n");
+    }
+
+    if(Y % 400 != 0 && Y % 100 == 0){
+        printf("Bukan Kabisat\n");
+    }
+
     return 0;
 }
-/* halo */
