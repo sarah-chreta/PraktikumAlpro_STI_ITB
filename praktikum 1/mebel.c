2945439d78be59b2fb9f12a6b8313548aef54479
sarah-chreta <sarahchreta@gmail.com>
2026-03-12 12:09:18 +0700

    feat: Mulai Mengerjakan Soal 4 Praktikum 1

---
+++ praktikum 1/mebel.c
@@ -0,0 +1,14 @@
+/* File : Pola Huruf M dengan Baris dan Kolom */
+#include <stdio.h>
+
+int main(){
+    /* KAMUS */
+    int tinggi;
+    int lebar;
+    int i;
+    int j;
+
+    /* ALGORITMA */
+    scanf("%d", &tinggi);
+    
+}
