fd5f9085990f4d394a53adead4913367ef5db755
sarah-chreta <sarahchreta@gmail.com>
2026-03-26 17:00:09 +0700

    feat: Memulai pemindaifrod

--- praktikum 2/pemindaifrod.c
+++ praktikum 2/pemindaifrod.c
@@ -1,3 +1,34 @@
 /* File : pemindaifrod.c */
 #include <stdio.h>
 
+int isPrime(int p){
+    int i;
+    if(p < 2){
+        return 0;
+    }
+
+    for(i = 2; i * i <= p; i++){
+        if(i % p == 0){
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+int main(){
+    long long P;
+    int D;
+    int jumlah;
+
+    scanf("%lld %d", &P, &D);
+    jumlah = 0;
+    for(long long i = P - D; i <= P + D; i++){
+        if(isPrime(i)){
+            jumlah++;
+        }
+    }
+
+    printf("%d\n", jumlah);
+    return 0;
+}
