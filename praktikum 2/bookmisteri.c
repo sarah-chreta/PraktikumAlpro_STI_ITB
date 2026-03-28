344ee457bd35aab4162cf185bc969f5739a749d1
sarah-chreta <sarahchreta@gmail.com>
2026-03-29 00:57:30 +0700

    feat: Final Soal bookmisteri

--- praktikum 2/bookmisteri.c
+++ praktikum 2/bookmisteri.c
@@ -5,27 +5,55 @@ int hitungDigit(int N){
     int jumlah;
 
     jumlah = 0;
-    for(int i = 0; i > 0; i++){
+    while(N > 0){
         j = N % 10;
         jumlah = jumlah + j;
-        j = j / 10;
+        N = N / 10;
     }
 
     return jumlah;
-}
+} // 615 --> 12
+
+int hitungJumlahDigit(int N){
+    int count;
+
+    count = 0;
+    while(N > 0){
+        count++;
+        N = N / 10;
+    }
+
+    return count;
+} // 615 --> 3
 
 int cariKode(int N){
     int jumlah = hitungDigit(N);
 
-    
+    int hasillangkah = jumlah;
+    for(int i = 0; i < hitungJumlahDigit(N); i++){
+        hasillangkah = hasillangkah * 10;
+    }
+
+    hasillangkah = hasillangkah + N;
+
+    int hasillangkah2 = hitungJumlahDigit(hasillangkah);
+
+    int finish = hasillangkah2;
+
+    for(int j = 0; j < hitungJumlahDigit(hasillangkah); j++){
+        finish = finish * 10;
+    }
+
+    finish = finish + hasillangkah;
+
+    return finish;
 }
 
 int main(){
     int n;
 
     scanf("%d", &n);
-
-    cariKode(n);
+    printf("%d\n", cariKode(n));
 
     return 0;
 }
