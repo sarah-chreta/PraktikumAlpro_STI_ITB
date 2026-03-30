edc5003093218ed2394b3b712b7446ad93d372b9
sarah-chreta <sarahchreta@gmail.com>
2026-03-30 18:19:17 +0700

    feat: Final Soal ez

---
+++ praktikum 3/ez.c
@@ -0,0 +1,24 @@
+/* File: ez.c */
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", &A[i]);
+    }
+
+    for(int i = N - 1; i >= 0; i--){
+        printf("%d", A[i]);
+        if(i != 0){
+            printf(" ");    // untuk menghasilkan spasi di output
+        }
+    }
+
+    printf("\n");
+    return 0;
+    
+}
