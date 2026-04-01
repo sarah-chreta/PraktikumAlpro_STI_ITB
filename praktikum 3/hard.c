e9a2454c67ab520bb9dc8acf5327321f662ade92
sarah-chreta <sarahchreta@gmail.com>
2026-04-01 21:39:54 +0700

    feat: Memulai Soal hard

---
+++ praktikum 3/hard.c
@@ -0,0 +1,14 @@
+/* File: hard.c */
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d", &N);
+
+    int A[N];
+    for(int i = 0; i < N; i++){
+        scanf("%d", &A[i]);
+    }
+
+    return 0;
+}
