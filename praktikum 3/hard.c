bec245fd4cb761d50dc501b8584cd7900ba410b3
sarah-chreta <sarahchreta@gmail.com>
2026-04-02 23:57:08 +0700

    feat: Menambah Soal hard

--- praktikum 3/hard.c
+++ praktikum 3/hard.c
@@ -1,6 +1,16 @@
 /* File: hard.c */
 #include <stdio.h>
 
+int digitCount(int x){
+    if(x == 0) return 1;
+    int count = 0;
+    while (x != 0){
+        count++;
+        x = x / 10;
+    }
+    return count;
+}
+
 int main(){
     int N;
     scanf("%d", &N);
