a39b5e7cb783b74de536f00887877c229bd43695
sarah-chreta <sarahchreta@gmail.com>
2026-04-04 07:35:43 +0700

    feat: Final Soal hard

--- praktikum 3/hard.c
+++ praktikum 3/hard.c
@@ -1,36 +1,41 @@
 /* File: hard.c */
 #include <stdio.h>
 
+int digitCount(int x){
+    if(x == 0) return 1;
+    int count = 0;
+    while (x != 0){
+        count++;
+        x = x / 10;
+    }
+    return count;
+}
+
 int main(){
     int N;
     scanf("%d", &N);
 
     int A[N];
+
     for(int i = 0; i < N; i++){
         scanf("%d", &A[i]);
     }
 
-    int Q;
-    scanf("%d", &Q);
+    for(int i = 0; i < N - 1; i++){
+        for(int j = 0; j < N - i - 1; j++){
+            int d1 = digitCount(A[j]);
+            int d2 = digitCount(A[j + 1]);
 
-    int start = 0;
-
-    for(int i = 0; i < Q; i++){
-        char C;
-        long long K;
-        scanf(" %c %lld", &C, &K);
-
-        K = K % N;
-
-        if(C == 'L'){
-            start = (start + K) % N;
-        } else if(C == 'R'){
-            start = (start - K + N) % N;
+            if(d1 < d2 || (d1 == d2 && A[j] > A[j + 1])){
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
         }
     }
 
     for(int i = 0; i < N; i++){
-        printf("%d", A[(start + i) % N]);
+        printf("%d", A[i]);
         if(i != N - 1) printf(" ");
     }
 
