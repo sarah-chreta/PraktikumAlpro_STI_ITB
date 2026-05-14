f8a92d3320cf8eacbb5936ce8566bc59b7d52368
sarah-chreta <sarahchreta@gmail.com>
2026-05-14 18:13:18 +0700

    feat: Memulai MesinKetik.c

---
+++ praktikum 5/MesinKetik.c
@@ -0,0 +1,14 @@
+#include "MesinKetik.h"
+
+void CreateList(ListKetik *L){
+    L->nEff = 0;
+    L->cursor = 0;
+}
+
+int length(ListKetik L){
+    return L.nEff;
+}
+
+void typeChar(ListKetik *L, ElType x){
+    
+}
