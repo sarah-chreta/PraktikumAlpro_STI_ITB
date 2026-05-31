e6e7e7a63a7d66846588bc02ab0d9ce66445107d
sarah-chreta <sarahchreta@gmail.com>
2026-05-31 16:43:07 +0700

    feat: Selesai soal minqueue

--- praktikum 7/soal3/minqueue.c
+++ praktikum 7/soal3/minqueue.c
@@ -23,10 +23,71 @@ boolean isMQFull(MinQueue mq){
 // trus masuk 2 -> bandingkan , keep 2
 // trus masuk 8 -> bandingkan 2 dan 8, keep 2, gituu
 void enqueue(MinQueue *mq, ElType val){
-    
+    push(&mq->sIn, val);
+
+    if(isEmpty(mq->sInMin)){
+        push(&mq->sInMin, val);
+    }
+    else{
+        ElType currentMin = TOP(mq->sInMin);
+
+        if(val < currentMin){
+            push(&mq->sInMin, val);
+        }
+        else{
+            push(&mq->sInMin, currentMin);
+        }
+    }
 }
 
-void dequeue(MinQueue *mq, ElType *val){
 
+void dequeue(MinQueue *mq, ElType *val){
+    if(isEmpty(mq->sOut)){
+
+        while(!isEmpty(mq->sIn)){
+            ElType x;
+            ElType dummy;
+
+            pop(&mq->sIn, &x);
+            pop(&mq->sInMin, &dummy);
+
+            push(&mq->sOut, x);
+
+            if(isEmpty(mq->sOutMin)){
+                push(&mq->sOutMin, x);
+            }
+            else{
+                ElType currentMin = TOP(mq->sOutMin);
+                if(x < currentMin){
+                    push(&mq->sOutMin, x);
+                }
+                else{
+                    push(&mq->sOutMin, currentMin);
+                }
+            }
+        }
+    }
+
+    pop(&mq->sOut, val);
+
+    ElType dummy;
+    pop(&mq->sOutMin, dummy);
+}
+
+ElType getMin(MinQueue mq){
+    if(isEmpty(mq.sIn)){
+        return TOP(mq.sOutMin);
+    }
+    else if(isEmpty(mq.sOut)){
+        return TOP(mq.sInMin);
+    }
+    else{
+        if(TOP(mq.sInMin) < TOP(mq.sOutMin)){
+            return TOP(mq.sInMin);
+        }
+        else{
+            return TOP(mq.sOutMin);
+        }
+    }
 }
 
